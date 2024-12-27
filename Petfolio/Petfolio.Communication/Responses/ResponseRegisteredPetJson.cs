92af12cd89aeee80621b3b781aa1f325f7e140ab
Fabricio da Silva Caetano <fscaetano@gmail.com>
2024-12-26 22:34:38 -0300

    Implemented first endpoint

---
+++ Petfolio/Petfolio.Communication/Responses/ResponseRegisteredPetJson.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Petfolio.Communication.Responses
+{
+    public class ResponseRegisteredPetJson
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+    }
+}
